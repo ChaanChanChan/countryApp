f9a448e0ecb3268a755f32507739395045628c9b
ChaanChanChan <alexandertakepics@gmail.com>
2023-07-25 13:07:42 +0200

    debounce search implemented in all pages, load data to local storage implemented, data persist on page

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
-
-type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania'
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
@@ -10,7 +9,7 @@ type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania'
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = []
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
@@ -19,6 +18,11 @@ export class ByRegionPageComponent {
   constructor(
     private countriesService: CountriesService
   ) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region
+  }
 
   searchByRegion(region: Region): void {
 
